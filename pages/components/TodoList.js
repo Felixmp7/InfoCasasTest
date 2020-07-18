0f65820f365aa70ce4ea8ebd16a8281fd659c0c4
Felix Pacheco <devfex.19@gmail.com>
2020-07-18 12:54:11 -0400

    implement list of todos - 10%

---
+++ pages/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Typography from "@material-ui/core/Typography";
+
+
+const TodoList = () => {
+  return (
+    <div>
+      <Typography variant="h6">Todo List</Typography>
+    </div>
+  );
+}
+
+export default TodoList
