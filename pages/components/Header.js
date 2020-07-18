c8da880b8395bbb558fddb38b7a0b68e4e892536
Felix Pacheco <devfex.19@gmail.com>
2020-07-18 12:53:48 -0400

    implement header - 80%

---
+++ pages/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../css/Header.module.css'
+import Typography from "@material-ui/core/Typography";
+
+const Header = ({data}) => {
+  console.log(data)
+  return (
+    <div className={styles.headerContainer}>
+      <Typography variant="h4">Welcome to InfoCasas Test</Typography>
+      <Typography variant="h5">{`Powered by <Felix Pacheco/>`}</Typography>
+    </div>
+  );
+}
+
+export default Header;
